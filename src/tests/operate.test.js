02c60b85b86d2166a0b53a3c821d1ba042000686
c00p75_ <georgecoopmsapenda@gmail.com>
2023-01-27 14:50:02 +0200

    Fix lint error. Add render test for quote component

---
+++ src/tests/operate.test.js
@@ -0,0 +1,16 @@
+import operate from '../logic/operate';
+
+describe('Test for operate function logic. It takes two numbers and the operator as argumenet', () => {
+  test('Return the sum of two numbers', () => {
+    expect(operate(10, 22, '+')).toEqual('32');
+  });
+  test('Return the difference of two numbers', () => {
+    expect(operate(1, 1, '-')).toEqual('0');
+  });
+  test('Multiply two numbers', () => {
+    expect(operate(62, 10, 'x')).toEqual('620');
+  });
+  test('Divide two numbers', () => {
+    expect(operate(9, 3, '÷')).toEqual('3');
+  });
+});
